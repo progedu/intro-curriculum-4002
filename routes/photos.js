b55b7b5f16a105787849bdd4e7adeb7bafd24203
hi60 <hiro.receive@gmail.com>
2017-03-09 03:24:38 +0000

    /photos以下にidが入力されるとWEBページがidを表示するよう実装

--- routes/photos.js
+++ routes/photos.js
@@ -6,4 +6,13 @@ router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
 
+router.param('id', (req, res, next, id) => {
+  res.send(`ようこそ！${id}さん！`);
+  next();
+});
+
+router.get('/:id', (req, res) => {
+  res.end();
+})
+
 module.exports = router;
