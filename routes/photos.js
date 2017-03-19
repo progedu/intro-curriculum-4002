401d955ae3a492cad67c70f05606b8a0f4ea85e6
tksfj17 <tksfj17@gmail.com>
2017-03-19 04:17:18 +0000

    res.sendをres.renderで書き換え、JADEで表示するように変更

--- routes/photos.js
+++ routes/photos.js
@@ -10,9 +10,14 @@ router.get('/', (req, res, next) => {
 * /photos/:title　のようなパスにアクセスがあった場合、
 * :title のところに入力された文字が
 * Webページに表示されるようにします。
-*/
+
 router.param('title', (req, res, next, title) => {
   res.send(title);
+});
+*/
+
+router.param('title', (req, res, next, title) => {
+  res.render('photos', { title: title });
 });
 
 router.get('/:title', (req, res, next) => {
