ec05e6b18ef9def4b5fae2501cf6a675de03a2c9
daitokuful <daitokuful@gmail.com>
2017-12-06 10:59:04 +0000

    /photos/hogeにアクセスした際にhogeと表示されるように変更

--- routes/photos.js
+++ routes/photos.js
@@ -6,4 +6,18 @@ router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
 
+router.param('title', (req, res, next, title) => {
+  // ここで、変数 title の内容をもとに存在チェック
+  if (title === 'hoge'){
+    res.send(title);
+  } else {
+    res.send('させるかあ!!');
+  }
+  next();
+});
+
+router.get('/:title', (req, res, next) => {
+  res.end();
+});
+
 module.exports = router;
