5d2d8273743fe5cec52a11a3ea4868e98f114933
homahi <homahi0128@gmail.com>
2017-12-12 20:11:56 +0900

    :titleを画面に表示できるように修正

--- routes/photos.js
+++ routes/photos.js
@@ -1,7 +1,13 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
-
+router.param('title', (req, res, next, id) => {
+  res.send(id);
+  next();
+});
+router.get('/:title', (req, res, next) => {
+  res.end();
+});
 router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
