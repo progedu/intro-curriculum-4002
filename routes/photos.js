d705bef876dbbc38ab6de303d501a8152f61a53c
m-sayuri2525 <frawreau@yahoo.co.jp>
2017-12-18 10:42:30 +0000

    入力されたパスの文字が、 Web ページに表示されるように実装

--- routes/photos.js
+++ routes/photos.js
@@ -2,8 +2,13 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.send('Some photos');
+router.param('id', (req, res, next, id) => {
+  res.send(id);
+  next();
+});
+
+router.get('/:id', (req, res, next) => {
+  res.send(`$id`);
 });
 
 module.exports = router;
