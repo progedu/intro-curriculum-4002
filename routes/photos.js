a5bbf82573370618e376ea2bceb55c07c2e2700b
m-sayuri2525 <frawreau@yahoo.co.jp>
2017-12-18 12:05:45 +0000

    バグ・フィックス

--- routes/photos.js
+++ routes/photos.js
@@ -2,13 +2,17 @@
 const express = require('express');
 const router = express.Router();
 
-router.param('id', (req, res, next, id) => {
-  res.send(id);
+router.get('/', (req, res, next) => {
+  res.send('Some photos');
+});
+
+router.param('title', (req, res, next, title) => {
+  res.send(title);
   next();
 });
 
-router.get('/:id', (req, res, next) => {
-  res.send(`$id`);
+router.get('/:title', (req, res, next) => {
+  res.end();
 });
 
 module.exports = router;
