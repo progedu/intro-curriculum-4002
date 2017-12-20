3284c00507c5a0a2669c8c9186541ae3d634148e
shunta071 <2095071@gmail.com>
2017-12-20 16:14:11 +0000

    hogeにアクセス時にhogeを表示

--- routes/photos.js
+++ routes/photos.js
@@ -6,4 +6,12 @@ router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
 
+router.param('id', (req, res, next, id) => {
+  res.send(id);
+});
+
+router.get('/:id', (req, res, next) => {
+  res.send(id);
+});
+
 module.exports = router;
