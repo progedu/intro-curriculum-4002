5b2c591cdd3173645317480799144c1a3418d878
kenkou0306 <kenkou0306@gmail.com>
2017-12-23 16:23:11 +0000

    レスポンスでタイトル表示

---
+++ routes/titles.js
@@ -0,0 +1,14 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+
+router.param('id', (req, res, next, id) => {
+  res.send(id);
+  next();
+});
+
+router.get('/:id', (req, res, next) => {
+  res.end();
+});
+
+module.exports = router;
