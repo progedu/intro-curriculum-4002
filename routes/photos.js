d8e367aa7dbbf2ef880c162f7378d80a16108eaf
Tatunori Obayashi <4210.tatu@gmail.com>
2018-01-30 14:24:36 +0000

    :title のところに入力された文字が Web ページに表示されるように変更

--- routes/photos.js
+++ routes/photos.js
@@ -2,8 +2,17 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+router.get('/',(req,res,next)=>{
   res.send('Some photos');
+});
+
+router.param('title',(req,res,next,id)=>{
+  res.send(id);
+  next();
+});
+
+router.get('/:title', (req, res, next) => {
+  res.end();
 });
 
 module.exports = router;
