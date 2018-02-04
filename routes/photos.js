abba632cc5a80aea3f038a37280210dfc5908f3c
n4cl <devn4cl@gmail.com>
2018-02-04 17:29:12 +0900

    パスのパラメータを出力する処理を追加

--- routes/photos.js
+++ routes/photos.js
@@ -1,6 +1,16 @@
 'use strict';
 const express = require('express');
+const xssFilters = require('xss-filters');
 const router = express.Router();
+
+router.param('id', (req, res, next, id) => {
+  res.send(xssFilters.inHTMLData(id));
+  next();
+});
+
+router.get('/:id', (req, res, next) => {
+  res.end();
+});
 
 router.get('/', (req, res, next) => {
   res.send('Some photos');
