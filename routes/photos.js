ed09a015ec7d70e673015b6681a8e576d5aaffce
s hakase <wal.hakase@gmail.com>
2018-02-19 10:44:04 +0000

    photosがパスのパラメータを受け取れるように変更

--- routes/photos.js
+++ routes/photos.js
@@ -6,4 +6,13 @@ router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
 
+router.param('title', (req, res, next, id) => {
+  res.send(id);
+  next();
+});
+
+router.get('/:title', (req, res, next) => {
+  res.end();
+});
+
 module.exports = router;
