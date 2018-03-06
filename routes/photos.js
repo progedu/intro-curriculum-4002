326e77a7d5a0ec7d4db0713555ef46a08e8f4d2c
Masashi Yoshitsugu <masashi_yosh@yahoo.co.jp>
2018-03-06 00:02:00 +0000

    修正

--- routes/photos.js
+++ routes/photos.js
@@ -2,8 +2,12 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res, next) => {
+  res.send('Some photos');
+});
+
 router.param('id', (req, res, next, id) => {
-	res.send(id);
+  res.send(id);
 	next();
 });
 
