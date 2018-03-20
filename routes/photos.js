62ccf326294adc480715b64c35d789de65d60f6b
tyokkaku <tyokkaku00@gmail.com>
2018-03-20 06:26:36 +0000

    URLで渡された文字列をページ内に表示

--- routes/photos.js
+++ routes/photos.js
@@ -6,4 +6,13 @@ router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
 
+router.param('hoge', (req, res, next, hoge) => {
+  res.send(hoge);
+  next();
+});
+
+router.get('/:hoge', (req, res, next) => {
+  res.end();
+});
+
 module.exports = router;
