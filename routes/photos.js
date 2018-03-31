76ec9284296b16e7f072da78df006debce5e6c20
Yuki Koyamatsu <wxiao919@gmail.com>
2018-03-31 14:09:57 +0000

    http://localhost:8000/photos/hoge にアクセスした際に hoge と表示されるように修正

--- routes/photos.js
+++ routes/photos.js
@@ -6,4 +6,13 @@ router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
 
+router.param('hoge',(req, res, next, title) => {
+  res.send(title);
+  next();
+});
+
+router.get('/hoge', (req, res, next) => {
+  res.send('hoge');
+});
+
 module.exports = router;
