478a389658e44256863536142938c21055b668a0
testcat2017 <32374452+testcat2017@users.noreply.github.com>
2018-04-01 14:13:17 +0000

    /photos/以下にアクセスがあった場合、そのURLパスの/photos/以下の文字列を画面に表示するよう変更

--- routes/photos.js
+++ routes/photos.js
@@ -2,8 +2,13 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.send('Some photos');
+router.param('title', (req, res, next, title) => {
+  res.send(title);
+  next();
+});
+
+router.get('/:title', (req, res, next) => {
+  res.end();
 });
 
 module.exports = router;
