0c6e167571f12c996d6801d046ce69a559166048
Masanori Funaki <funakimfunaki@gmail.com>
2018-04-06 05:36:09 +0000

    練習問題の実装

--- routes/photos.js
+++ routes/photos.js
@@ -6,4 +6,14 @@ router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
 
+router.param('title', (req, res, next, title) => {
+  console.log(title);
+  // console.log(title);
+  res.end(title);
+});
+
+router.get('/:title', (req, res, next) => {
+  res.end();
+});
+
 module.exports = router;
