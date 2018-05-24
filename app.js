1735b3be9c24ade7b7ca0de7da987bcbf1e86dfc
DDRAGON <davide.0829@gmail.com>
2018-05-24 14:19:49 +0900

    Express のアップデート by 一括変換ツール

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var helmet = require('helmet');
 
-var routes = require('./routes/index');
+var index = require('./routes/index');
 var users = require('./routes/users');
 var photos = require('./routes/photos');
 
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/', index);
 app.use('/users', users);
 app.use('/photos', photos);
 
