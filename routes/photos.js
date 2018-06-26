8f367ac72572ef264b4bbe4edf5e8977efffce58
hase1108 <hase1108shoiti@yahoo.co.jp>
2018-06-26 12:27:43 +0000

    練習問題

--- routes/photos.js
+++ routes/photos.js
@@ -6,4 +6,12 @@ router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
 
+router.param('hoge', (req, res, next, hoge) => {
+  res.send(hoge);
+  next();
+});
+
+router.get('/:hoge', (req, res, next) => {
+  res.end();
+})
 module.exports = router;
