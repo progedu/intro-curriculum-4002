479d97da7b546689b09624cbed64ca77da8597ef
sironekotoro <develop@sironekotoro.com>
2018-06-27 11:18:02 +0000

    photosの後の文字列をページに表示

--- routes/photos.js
+++ routes/photos.js
@@ -2,8 +2,14 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.send('Some photos');
+router.get('/:id', (req, res, next) => {
+  res.end();
+});
+
+
+router.param('id', (req, res, next, id) => {
+  res.send(id);
+  next;
 });
 
 module.exports = router;
