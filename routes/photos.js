b85a49dba3ebfe9849e512f41a297a7ca4b0ff85
Takumi Sameshima <vanxs24@gmail.com>
2018-07-29 15:17:50 +0000

    パスのパラメーターを表示

--- routes/photos.js
+++ routes/photos.js
@@ -5,5 +5,12 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
+router.param('title', (req, res, next, title) => {
+  res.send(title);
+  next();
+});
+router.get('/:title', (req, res, next) => {
+  res.end();
+});
 
 module.exports = router;
