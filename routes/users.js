cdb315aa004060ce3c147aee18bdaeb052e9e9c3
kuwahara0902 <kuwahara-@live.jp>
2018-08-19 12:22:53 +0000

    :title のところに入力された文字が Web ページに表示

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.param('title', (req, res, next, title) => {
+  res.send(title);
+  next();
+});
+
+router.get('/:title', (req, res, next) => {
+  res.end();
+});
+
 module.exports = router;
