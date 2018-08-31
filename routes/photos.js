4c654f3bda620958d34d1bdec511a1e609abcf25
capybaradesuga <capybaradesuga@gmail.com>
2018-08-31 23:52:54 +0000

    photosにパスパラメータを実装

--- routes/photos.js
+++ routes/photos.js
@@ -2,8 +2,18 @@
 const express = require('express');
 const router = express.Router();
 
+
 router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
+
+router.param('title', (req, res, next, title) => {
+  res.end(title);
+  next();
+});
+
+router.get('/:title', (req, res, next) => {
+  res.end();
+})
 
 module.exports = router;
