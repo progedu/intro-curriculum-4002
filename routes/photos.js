908e0c2f2d516a603879aa70fdd04382a4eae3d4
yazawa_yoshihide <yazawabjj@gmail.com>
2018-09-09 17:45:08 +0000

    練習問題の実施

--- routes/photos.js
+++ routes/photos.js
@@ -2,6 +2,13 @@
 const express = require('express');
 const router = express.Router();
 
+router.param('title',(req, res, next, title)=> {
+  res.send(title);
+  next();
+})
+router.get('/:title', (req, res, next) => {
+  res.end();
+})
 router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
