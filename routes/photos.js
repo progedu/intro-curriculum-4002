3a6ab0929cf66e0702c50ec6eafdfe30f338e8e0
takedaxmasayuki <takeda_masayuki@crossware.co.jp>
2018-09-17 13:28:52 +0000

    ルーティング処理を追加

--- routes/photos.js
+++ routes/photos.js
@@ -6,4 +6,15 @@ router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
 
+router.param('title', (req, res, next, title) => {
+  res.send(title);
+  next();
+});
+
+router.get('/:title', (req, res, next) => {
+  res.end();
+});
+
+router.get('')
+
 module.exports = router;
