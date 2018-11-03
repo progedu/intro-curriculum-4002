d8c32a986ce4b8ccd5dc527e0e06b798c6c5bd10
kansuke224 <kansuke224224@gmail.com>
2018-11-03 01:05:58 +0000

    パスのパラメーターを表示するよう修正

--- routes/photos.js
+++ routes/photos.js
@@ -2,8 +2,18 @@
 const express = require('express');
 const router = express.Router();
 
+
 router.get('/', (req, res, next) => {
   res.send('Some photos');
+});
+
+router.param('id', (req, res, next, id) => {
+  res.send(id);
+  next();
+});
+
+router.get('/:id', (req, res, next) => {
+  res.end();
 });
 
 module.exports = router;
