0147bf8eb89284d8c0020b72897ff8dbad00ea95
nonota7788 <f.t.fs0924@gmail.com>
2019-02-08 03:30:07 +0000

    urlにパラメータの指定がある場合、そのパラメーターをブラウザに表示するよう実装

--- routes/photos.js
+++ routes/photos.js
@@ -6,4 +6,19 @@ router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
 
+router.param('id', (req, res, next, id) => {
+  if (id === 'hoge') {
+    res.send(id);
+    next();
+  } else {
+    res.send('未対応のパラメーターです');
+  }
+});
+  
+
+
+router.get('/:id', (req, res, next) => {
+  res.end();
+});
+
 module.exports = router;
