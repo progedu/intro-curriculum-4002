b626c39dae4beab70baee4a7de6642c605ac27e5
Kouhei Hamano <marco.fronsys@gmail.com>
2019-03-01 06:27:00 +0000

    /photos/:titleの:以降の文字を表示されるよう変更

--- routes/photos.js
+++ routes/photos.js
@@ -6,4 +6,14 @@ router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
 
+// TODO Q1.XSS脆弱性の対策を行え
+router.param('id', (req, res, next, id) => {
+  res.send(id);
+  next();
+});
+
+router.get('/:id', (req, res, next) => {
+  res.end();
+});
+
 module.exports = router;
