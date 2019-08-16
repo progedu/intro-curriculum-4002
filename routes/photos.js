889c58d91cbb524314d544787934d4300b3e68c4
umikz <nanjo@umikz.com>
2019-08-17 06:27:18 +0900

    photos以下のパスを表示

--- routes/photos.js
+++ routes/photos.js
@@ -6,4 +6,10 @@ router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
 
+// パスのパラメータのidという項目を、req.param.idで受け取り
+// send関数を使って表示
+router.get('/:id', (req, res, next) => {
+  res.send(req.params.id);
+});
+
 module.exports = router;
