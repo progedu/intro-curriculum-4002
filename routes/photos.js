a820cce1963e3fc8b7dba4d485b1e7da5a8a3e18
katana2019 <1031yoshi@jcom.zaq.ne.jp>
2019-12-20 23:35:43 +0900

    4-2 練習問題の回答

--- routes/photos.js
+++ routes/photos.js
@@ -5,5 +5,9 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
+router.get('/:title', (req, res, next) => {
+  res.header('Content-Type', 'text/plain; charset=utf-8')
+  res.send(req.params.title);
+});
 
 module.exports = router;
