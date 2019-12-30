a6234b37c3d1ad2f8a421e791541398f1d797df5
Hiroki Motooka <hiroki161@gmail.com>
2019-12-31 03:05:02 +0900

    Fixed XSS vulnerability

--- routes/photos.js
+++ routes/photos.js
@@ -7,7 +7,9 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/:id', (req, res, next) => {
-  res.send(req.params.id);
+  var id = req.params.id;
+  id = id.replace(/\&/g, "&amp;").replace(/\"/g, '&quot;').replace(/\</g, "&lt;").replace(/\>/g, "&gt;");
+  res.send(`${id}`);
 });
 
 module.exports = router;
