83e7a5dceb2dd9fc25294d6b1d49b39de994d280
Kazuyuki Matsuda <mtdkzyk.github@gmail.com>
2020-02-02 16:09:27 +0900

    練習問題の指定した変数名に変更

--- routes/photos.js
+++ routes/photos.js
@@ -6,8 +6,9 @@ router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
 
-router.get('/:id', (req, res, next) => {
-  res.send(req.params.id);
+router.get('/:title', (req, res, next) => {
+  res.send(req.params.title);
+  // ここで、変数 title の内容をもとに存在チェック
 });
 
 module.exports = router;
