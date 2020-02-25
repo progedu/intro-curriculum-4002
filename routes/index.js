d8e09afd7c2aa8aaf993fda5af1d1549ed622ea1
Gorilla <log1230gori@outlook.jp>
2020-02-25 13:15:23 +0900

    練習問題実装

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/:id', function(req,res, next) {
+  res.send(req.params.id);
+})
 
 module.exports = router;
--- routes/photos.js
+++ routes/photos.js
@@ -6,4 +6,12 @@ router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
 
+router.get('/:id', (req,res, next) => {
+  res.send(req.params.id);
+})
+router.get('/:title', (req,res,next) => {
+  const title= req.params.title;
+  //処理
+})
+
 module.exports = router;
