bdf902520b3504cb2be93798baa2a19bed63aa90
michihiko0419 <michi0419torokuyo@gmail.com>
2020-03-08 01:17:38 +0000

    display params

--- routes/photos.js
+++ routes/photos.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.send('Some photos');
+router.get('/:id', (req, res, next) => {
+  res.send(req.params.id);
 });
 
 module.exports = router;
