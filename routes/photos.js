d5874c792b9f8dabdb80e881843b421927ce16a2
Noriki Kunugi <no-kunugi@gmail.com>
2020-04-13 10:59:48 +0900

    photosルートへアクセス時の処理を追加

--- routes/photos.js
+++ routes/photos.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res, next) => {
+  res.send('Some photos');
+});
+
 router.get('/:title', (req, res, next) => {
   res.send(req.params.title);
 });
