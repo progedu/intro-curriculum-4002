0b0a5e8518661d06f3f5483cea6dad4affbd7df9
leavleaf <leavleaf@gmail.com>
2020-04-23 12:09:43 +0900

    :title のところに入力された 文字が Web ページに表示されるように実装

--- routes/photos.js
+++ routes/photos.js
@@ -1,9 +1,13 @@
-'use strict';
-const express = require('express');
+"use strict";
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.send('Some photos');
+router.get("/", (req, res, next) => {
+  res.send("Some photos");
+});
+
+router.get("/:id", (req, res, next) => {
+  res.send(req.params.id);
 });
 
 module.exports = router;
