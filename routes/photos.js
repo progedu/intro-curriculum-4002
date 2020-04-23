ba7e49a705666f117cef89c081c27db1379b88c8
kuniyasu <ryo_k_1014@yahoo.co.jp>
2020-04-23 16:34:11 +0900

    入力文字の表示

--- routes/photos.js
+++ routes/photos.js
@@ -6,8 +6,8 @@ router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
 
-router.get('/:id', (req, res, next) => {
-  res.send(req.params.id);
+router.get('/:title', (req, res, next) => {
+  res.send(req.params.title);
 });
 
 module.exports = router;
