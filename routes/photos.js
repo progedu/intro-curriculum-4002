327e99bb63d6def7a04c990db4c05b8cfaf55f13
cosimafromhell <fjhshrm@gmail.com>
2020-04-23 16:11:37 +0900

    回答に一部誤りがあったため修正

--- routes/photos.js
+++ routes/photos.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/:id', (req, res, next) => {
-  res.send(req.params.id);
+router.get('/:title', (req, res, next) => {
+  res.send(req.params.title);
 });
 
 module.exports = router;
