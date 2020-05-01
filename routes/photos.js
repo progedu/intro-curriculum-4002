387525ed105dfb8abd06d8fb96fd62475a4b555c
orioncola <13391snyth@gmail.com>
2020-05-01 22:08:09 +0900

    アクセスパスを表示する対応

--- routes/photos.js
+++ routes/photos.js
@@ -6,4 +6,9 @@ router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
 
+router.get('/:id', (req, res, next) => {
+  res.header('Content-Type', 'text/plain;charset=utf-8');
+  res.send(req.params.id);
+});
+
 module.exports = router;
