095153f353e3579d0c34ac91077318c0d61dfb1d
Shumpei Fukui <shunpei.f.0408@gmail.com>
2020-05-08 22:46:33 +0900

    パスの一部をwebページに表示する修正

--- routes/photos.js
+++ routes/photos.js
@@ -4,6 +4,10 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   res.send('Some photos');
+})
+
+router.get('/:id', (req, res, next) => {
+  res.send(req.params.id);
 });
 
 module.exports = router;
