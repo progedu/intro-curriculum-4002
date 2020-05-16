289332a565fbf6d7729d51b1200f9a016155155c
mitsuki <yanasemistuki1203@gmail.com>
2020-05-17 02:23:57 +0900

    修正

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/photos', photosRouter);
 
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
