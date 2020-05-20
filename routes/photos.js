06bc51605bd41ae64bbbd1095c5b563716890233
zavi3 <duna.blue@gmail.com>
2020-05-20 12:58:51 +0900

    不正なタグ付きアクセスは404に変更

--- routes/photos.js
+++ routes/photos.js
@@ -6,4 +6,10 @@ router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
 
+router.get('./:title', (err, req, res, next) =>{
+  const title = req.params.title;
+  title.includes('&lt;script&gt; alert (\'test\'); &lt;/script&gt;') ? err : {};
+  res.send(title);
+});
+
 module.exports = router;
