088c58affd0ea493c46459cb4567f59e0818ab13
hiroin <egamescape@gmail.com>
2020-05-21 22:09:26 +0900

    Create the screen for /photos/:title

--- routes/photos.js
+++ routes/photos.js
@@ -6,4 +6,12 @@ router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
 
+router.get('/:title', function(req, res, next) {
+  res.render('photos', { title: req.params.title });
+});
+
+router.get('/:title', (req, res, next) => {
+  res.send(req.params.title);
+});
+
 module.exports = router;
