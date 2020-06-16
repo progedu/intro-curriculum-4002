13f9bd37e80683577a8d225fbb3b8a36c2423d71
Tomoya Kanemaru <pandora.border@gmail.com>
2020-06-17 00:30:12 +0900

    URLの文字列をHTMLに表示するように改修

--- routes/photos.js
+++ routes/photos.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.send('Some photos');
+router.get('/:path', (req, res, next) => {
+  res.send(req.params.path);
 });
 
 module.exports = router;
