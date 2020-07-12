bd6c95e06f4529994d3471569fc10d3215d47ad6
pjbassspecial <pjbass1973@gmail.com>
2020-07-12 20:44:44 +0900

    hoge

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/photos', photosRouter);
-
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
