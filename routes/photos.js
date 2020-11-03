d53d6cd5c0c87e414e39167a2308a5752f425b9a
parakeet15 <kaisei_19n1102563@nnn.ed.jp>
2020-11-03 23:10:06 +0900

    パスのパラメーターを表示するように実装

--- routes/photos.js
+++ routes/photos.js
@@ -6,4 +6,9 @@ router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
 
+router.get('/:title', (req, res, next) => {
+  res.header('Content-Type', 'text/plain; charset=utf-8');
+  res.send(req.params.title);
+});
+
 module.exports = router;
