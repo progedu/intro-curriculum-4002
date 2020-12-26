b317c5bce9023658fd39f31da125427899d44574
Jonathan <jonathanjoster0331@gmail.com>
2020-12-26 15:40:25 +0900

    completed

--- routes/photos.js
+++ routes/photos.js
@@ -5,5 +5,8 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
+router.get('/:id', (req, res, next) => {
+  res.send(req.params.id);
+});
 
 module.exports = router;
