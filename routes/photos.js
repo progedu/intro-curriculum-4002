95c4de7380a06cdc33b2a755031d4d1d7cfe9486
shinfujita <shin4522@gmail.com>
2021-01-08 13:57:26 +0900

    4章2節の課題

--- routes/photos.js
+++ routes/photos.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/:title', (req, res, next) => {
-  res.send(req.params.title);
+router.get('/:id', (req, res, next) => {
+  res.send(req.params.id);
 });
 
 module.exports = router;
