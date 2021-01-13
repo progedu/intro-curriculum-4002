d907f2e0adc2ca39acd7ca2d3c4ec28e72515300
HappyKumamoto <zen.word+hub@gmail.com>
2021-01-14 03:23:03 +0900

    パスの文字列を表示させる

--- routes/photos.js
+++ routes/photos.js
@@ -2,8 +2,15 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.send('Some photos');
+//router.get('/', (req, res, next) => {
+//  res.send('Some photos');
+//});
+
+router.get('/:title', (req, res, next) => {
+  //以下、脆弱性対策3つ
+  //const title = req.params.title;// ここで、変数 title の内容をもとに存在チェック
+  //res.header('Content-Type', 'text/plan;charset=utf-8');//プレーンテキストとして
+  res.json(req.params.title);//Json形式で返す
 });
 
 module.exports = router;
