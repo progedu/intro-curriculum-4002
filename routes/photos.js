1e04456c897ddf3990772755ac94d49ae9c19abe
housinsen <mtt6gra@gmail.com>
2021-01-15 20:13:29 +0900

    練習問題

--- routes/photos.js
+++ routes/photos.js
@@ -2,8 +2,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+router.get('/:title', (req, res, next) => {
   res.send('Some photos');
 });
+
+router.get('/:title', (req, res, next) => {
+    res.send(req.params.title);
+  });
 
 module.exports = router;
