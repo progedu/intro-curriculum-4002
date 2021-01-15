9889f127554a40eb7ef7818050b3484d7c9c4314
vagrant <vagrant@vagrant.vm>
2021-01-15 20:10:50 +0900

    pug対応

--- routes/photos.js
+++ routes/photos.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/:title', (req, res, next) => {
-  res.send(req.params.title);
+router.get('/:id', (req, res, next) => {
+  res.render('photos', { title: req.params.id });
+  //res.send(req.params.title);
 });
-
 module.exports = router;
