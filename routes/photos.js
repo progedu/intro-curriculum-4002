2de2b9a4b766d7eb40c17b46bfdd254084e72340
yoh <rapture6040@gmail.com>
2021-01-15 20:25:00 +0900

    練習問題

--- routes/photos.js
+++ routes/photos.js
@@ -7,7 +7,7 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/:title', (req, res, next) => {
-  const title = req.params.title;
+  res.send(req.params.title);
 });
 
 module.exports = router;
