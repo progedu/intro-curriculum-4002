d05788b40d688b43c9e205b1e28b666336b936e5
yamamoto <yamamoto2852@gmail.com>
2021-01-16 09:30:42 +0900

    パス名が表示される機能の実装

--- routes/photos.js
+++ routes/photos.js
@@ -5,5 +5,8 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
+router.get('/:hoge', (req, res, next) => {
+  res.send(req.params.hoge);
+});
 
 module.exports = router;
