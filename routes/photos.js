b9cbfaae9ebf59d0ab3095bf172d515a21049771
Kenichi Oosaki <kenichi_oosaki@dwango.co.jp>
2021-01-20 19:34:35 +0900

    パスの表示対応

--- routes/photos.js
+++ routes/photos.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.send('Some photos');
+router.get('/:title', (req, res, next) => {
+  res.send(req.params.title);
 });
 
 module.exports = router;
