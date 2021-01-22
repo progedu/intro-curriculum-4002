93a50951e4a05c2cc1b00a0619faaf589c73c8fd
samurai <xcybza155@gmail.com>
2021-01-22 16:09:23 +0900

    パスの表示

--- routes/photos.js
+++ routes/photos.js
@@ -2,8 +2,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.send('Some photos');
+router.get('/nnn_logo', (req, res, next) => {
+  // res.send(req.params.title);
+  res.render('photos', { title: req.params.id });
 });
 
 module.exports = router;
