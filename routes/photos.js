b4ef51e297b9b9388d5bf7fbc9dba59837aa257f
nooneknows2020 <e_dends@yahoo.co.jp>
2021-02-10 15:05:20 +0900

    スクリプトを埋め込まれないように変更

--- routes/photos.js
+++ routes/photos.js
@@ -8,7 +8,12 @@ router.get('/', (req, res, next) => {
 
 // URLに含まれる文字列(:title)をページに表示
 router.get('/:title', (req, res, next) => {
-  res.send(req.params.title);
+  // スクリプトを埋め込まれないように変更
+  res.writeHead(200, {
+    'Content-type': 'text/plain; charset=utf-8'
+  });
+  res.write(req.params.title);
+  res.end();
 });
 
 module.exports = router;
