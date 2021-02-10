7c0bcdf2f5f51fccf0d2778a35f2b5da0b660013
nooneknows2020 <e_dends@yahoo.co.jp>
2021-02-10 14:44:56 +0900

    URLに含まれる文字列(:title)をページに表示

--- routes/photos.js
+++ routes/photos.js
@@ -6,4 +6,9 @@ router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
 
+// URLに含まれる文字列(:title)をページに表示
+router.get('/:title', (req, res, next) => {
+  res.send(req.params.title);
+});
+
 module.exports = router;
