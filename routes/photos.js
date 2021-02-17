ad00d2d7388de9d6c7163a806086518035cf94d8
gomi <assassincreed0461-gta005@outlook.jp>
2021-02-17 22:25:45 +0900

    テスト出来ました。

--- routes/photos.js
+++ routes/photos.js
@@ -6,4 +6,14 @@ router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
 
+// テスト問題
+router.get('/:title', (req, res, next) => {
+  res.send(req.params.title);
+});
+
+// 画像表示
+/*router.get('/:id', (req, res, next) => {
+  res.render('index', {image: req.params.id});
+})*/
+
 module.exports = router;
