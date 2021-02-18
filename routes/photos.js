f77bc72da6d68a89597407c37830f99dd536eba4
sshoudai <50400813+sshoudai@users.noreply.github.com>
2021-02-18 16:32:07 +0900

    :titleパスの対応

--- routes/photos.js
+++ routes/photos.js
@@ -2,8 +2,25 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.send('Some photos');
+// router.get('/', (req, res, next) => {
+//   res.send('Some photos');
+// });
+
+// router.get('/:id', (req, res, next) => {
+//   res.send(req.params.id);
+// });
+
+// router.get('/:title', (req, res, next) => {
+//   const title = req.params.title;
+//   res.send(title);
+// });
+router.get('/:title', (req, res, next) => {
+  // スクリプトを埋め込まれないように変更
+  res.writeHead(200, {
+    'Content-type': 'text/plain; charset=utf-8'
+  });
+  res.write(req.params.title);
+  res.end();
 });
 
 module.exports = router;
