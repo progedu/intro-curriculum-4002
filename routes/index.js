27d9eacc53717c4caf79cbf2da9d8b7aa226a593
Ryosuke SEMBA <1o12ryo@gmail.com>
2021-03-05 17:43:36 +0900

    photoの下ではなく、同列にならべてしまったが。。

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* */
+router.get('/:id', (req, res, next) => {
+  res.send(req.params.id);
+});
+
+
 module.exports = router;
