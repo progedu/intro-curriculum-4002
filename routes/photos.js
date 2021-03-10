3b762ab31edaf7aac4b9f482983c56cc2faed4f4
Ryuta Watanabe <watanabe@initinc.co.jp>
2021-03-10 16:09:40 +0900

    /photos/:title へのアクセス時、:titleの文字列をWebページに表示されるように修正

--- routes/photos.js
+++ routes/photos.js
@@ -1,9 +1,9 @@
-'use strict';
-const express = require('express');
+"use strict";
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.send('Some photos');
+router.get("/:title", (req, res, next) => {
+  res.send(req.params.title);
 });
 
 module.exports = router;
