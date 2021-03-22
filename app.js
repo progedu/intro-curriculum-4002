fa219323a514f6f73d70e21644cf2eaa2da4991b
Aoi Yamaguchi <yamaguchiaoi0130@gmail.com>
2021-03-22 16:30:26 +0900

    hogeの追加

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var photosRouter = require('./routes/photos');
 
+//追加
+var hogeRouter = require('./routes/hoge');
+
 var app = express();
 app.use(helmet());
 
@@ -25,6 +28,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/photos', photosRouter);
+
+//追加
+app.use('/hoge', hogeRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
