73510e3c9c736c307994bd4f3c87b23b90360b88
satopoppo <satona.oinuma@is-tech.co.jp>
2021-06-05 14:04:13 +0900

    hoge表示に変更

--- routes/photos.js
+++ routes/photos.js
@@ -2,8 +2,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.send('Some photos');
+router.get('/:id', (req, res, next) => {
+  res.send(req.params.id);
+  //ここでパスで指定されたパラメータが、実際に存在するものか確認することでXSS脆弱性の対策ができる
 });
 
 module.exports = router;
